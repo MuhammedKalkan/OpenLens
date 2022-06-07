5ce51ad1e56235631fa9c7a9ebfac8f661efbf92
Muhammed Kalkan <muhammed.kalkan@tubitak.gov.tr>
2022-06-07 10:18:55 +0300

    Update update.js

--- update.js
+++ update.js
@@ -3,9 +3,8 @@ const fs = require('fs');
 var packagejson = require('./package.json');
 
 packagejson.build.publish = {
-    url: "https://github.com/MuhammedKalkan/OpenLens/releases/download/latest",
-    provider: "generic",
-    channel: "latest"
+    url: "https://github.com/MuhammedKalkan/LensPrivate/releases/download/Latest",
+    provider: "generic"
 }
 
 fs.writeFileSync('package.json', JSON.stringify(packagejson));
