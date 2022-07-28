9b4b010f576a6746575514e5e699bef776d7b3d1
Muhammed Kalkan <kalkanikbal@gmail.com>
2022-07-28 19:41:16 +0300

    Configure windows build artifact name
    Auto updater name convention fix

--- update.js
+++ update.js
@@ -5,6 +5,8 @@ var packagejson = require('./package.json');
 packagejson.build.publish = {
     url: "https://github.com/MuhammedKalkan/OpenLens/releases/download/Latest",
     provider: "generic"
-}
+};
+
+packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
 
 fs.writeFileSync('package.json', JSON.stringify(packagejson));
