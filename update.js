d66845b79fc8f5ddcfe279f48d186a473c761239
Jan-Otto Kröpke <mail@jkroepke.de>
2023-05-31 00:02:48 +0200

    Implement Build Number (#149)

--- update.js
+++ update.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 
-var packagejson = require('./lens/open-lens/package.json');
+const packageJsonOpenLens = require('./lens/open-lens/package.json');
 
-packagejson.build.publish = [{
+packageJsonOpenLens.build.publish = [{
     url: "https://github.com/MuhammedKalkan/OpenLens/releases/download/Latest",
     provider: "generic"
 }];
 
-packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
-packagejson.build.npmRebuild = true;
+packageJsonOpenLens.version = `${packageJsonOpenLens.version}+${process.env.BUILD_NUMBER}`;
+packageJsonOpenLens.build.artifactName = "${productName}-${buildVersion}-${arch}-${os}.${ext}";
+packageJsonOpenLens.build.linux.artifactName = packageJsonOpenLens.build.linux.artifactName.replace("${version}", "${buildVersion}");
+packageJsonOpenLens.build.win.artifactName = "OpenLens.Setup.${buildVersion}.${ext}";
+packageJsonOpenLens.build.npmRebuild = true;
 
-fs.writeFileSync('./lens/open-lens/package.json', JSON.stringify(packagejson));
+console.log(`Set build version: ${packageJsonOpenLens.version}`)
+
+fs.writeFileSync('./lens/open-lens/package.json', JSON.stringify(packageJsonOpenLens, null, 2));
