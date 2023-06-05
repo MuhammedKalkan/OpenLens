7c226405b1778a23b754ed778d75602dfc566c76
Jan-Otto Kröpke <mail@jkroepke.de>
2023-06-05 18:18:01 +0200

    Mention binary application builds (#153)

--- update.js
+++ update.js
@@ -9,6 +9,12 @@ packageJsonOpenLens.build.publish = [{
 
 packageJsonOpenLens.version = `${packageJsonOpenLens.version}-${process.env.BUILD_NUMBER}`;
 packageJsonOpenLens.build.npmRebuild = true;
+packageJsonOpenLens.copyright = [
+    packageJsonOpenLens.copyright,
+    '',
+    'Binary application builds @ MuhammedKalkan/OpenLens',
+    'by Muhammed Kalkan, Jan-Otto Kröpke, Ebby Peter, Xaver Lohmüller'
+].join("\r\n")
 
 console.log(`Set build version: ${packageJsonOpenLens.version}`)
 
