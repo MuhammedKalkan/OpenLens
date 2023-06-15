68fc6c01c303c464928d1cf2738bf10ab71c1d51
Jan-Otto Kröpke <mail@jkroepke.de>
2023-06-15 10:01:04 +0200

    debug

--- update.js
+++ update.js
@@ -13,7 +13,7 @@ packageJsonOpenLens.build.npmRebuild = os.platform() === 'darwin';
 packageJsonOpenLens.build.detectUpdateChannel = false;
 packageJsonOpenLens.devDependencies["electron-builder"] = "^24.4.0"
 
-packageJsonOpenLens.scripts.postinstall = "electron-builder install-app-deps"
+delete packageJsonOpenLens.scripts.postinstall;
 
 packageJsonOpenLens.copyright = [
     packageJsonOpenLens.copyright,
